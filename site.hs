e73391dfe69ce9815e2ed96e2bc19d3433a1bfc0
Beerdude26 <Beerdude26@gmail.com>
2015-07-24 23:57:27 +0200

    Renamed home page title

--- site.hs
+++ site.hs
@@ -47,7 +47,7 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
+                    constField "title" "Empires Mod"         `mappend`
                     defaultContext
 
             getResourceBody
